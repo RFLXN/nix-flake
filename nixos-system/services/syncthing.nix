c4e1704eb7944bb89db02d07960f3ba5adfc6dd7
RFLXN <solid2113@naver.com>
2026-01-30 03:28:54 +0900

    2026-01-30::v2

--- nixos-system/services/syncthing.nix
+++ nixos-system/services/syncthing.nix
@@ -1,26 +1,24 @@
 { username, hostNames, ... }: {
-  home-manager.users.${username} = {
-    services.syncthing = {
-      enable = true;
+  services.syncthing = {
+    enable = true;
 
-      settings = {
-        devices = {
-          "rflxn-macbook" = { id = "UUNNXI2-S5LHREV-GVIDWMB-RBZ5PSO-EQ7LRSY-TDQP3BG-EQAK3XK-ZXEFTQF"; };
-          "rflxn-desktop" = { id = "63DJZE5-GC3RSIV-UANZZEE-O4JL4EV-4CDOGPD-HCSDGHP-ZX4XFRI-ZA47DAM"; };
+    settings = {
+      devices = {
+        "rflxn-macbook" = { id = "UUNNXI2-S5LHREV-GVIDWMB-RBZ5PSO-EQ7LRSY-TDQP3BG-EQAK3XK-ZXEFTQF"; };
+        "rflxn-desktop" = { id = "63DJZE5-GC3RSIV-UANZZEE-O4JL4EV-4CDOGPD-HCSDGHP-ZX4XFRI-ZA47DAM"; };
+      };
+      
+      folders = {
+        "development" = {
+          id = "development";
+          path = "~/development";
+          devices = [ "rflxn-macbook" "rflxn-desktop" ];
         };
 
-        folders = {
-          "development" = {
-            id = "development";
-            path = "~/development";
-            devices = [ "rflxn-macbook" "rflxn-desktop" ];
-          };
-
-          "data" = {
-            id = "data";
-            path = "~/data";
-            devices = [ "rflxn-desktop" ];
-          };
+        "data" = {
+          id = "data";
+          path = "~/data";
+          devices = [ "rflxn-desktop" ];
         };
       };
     };

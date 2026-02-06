e19f7398d2e1a286bb9a29b9adfbadf8bb5b973b
RFLXN <solid2113@naver.com>
2026-02-07 02:35:25 +0900

    2026-02-07::v4

--- nixos-system/services/docker/default.nix
+++ nixos-system/services/docker/default.nix
@@ -2,9 +2,11 @@
   virtualisation.docker = {
     enable = true;
     storageDriver = "btrfs";
+    enableOnBoot = true;
 
     daemon.settings = {
       dns = [ "1.1.1.1" "1.0.0.1" ];
+      iptables = true;
     };
   };
 

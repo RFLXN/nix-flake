41e84eb0d0856b419e9b49d3fe7510858d8a4075
RFLXN <solid2113@naver.com>
2026-03-17 01:36:57 +0900

    2026-03-17::v2

--- modules/services/deluge/default.nix
+++ modules/services/deluge/default.nix
@@ -7,6 +7,7 @@
 {
   config,
   lib,
+  pkgs,
   defaultPersistPath ? null,
   username,
   ...
@@ -14,6 +15,7 @@
 let
   path = if persistPath != null then persistPath else defaultPersistPath;
   dataDir = "/var/lib/deluge";
+  configDir = "${dataDir}/.config/deluge";
   configuredGroup = lib.attrByPath [ "users" "users" username "group" ] "" config;
   primaryGroup = if configuredGroup != "" then configuredGroup else "users";
 
@@ -24,6 +26,8 @@ let
     allow_remote = true;
     download_location = effectiveDownloadDir;
   };
+
+  generatedCoreConf = pkgs.writeText "core.conf" (builtins.toJSON baseConfig);
 in
 lib.mkMerge [
   {
@@ -32,7 +36,7 @@ lib.mkMerge [
       user = username;
       group = primaryGroup;
       inherit dataDir;
-      declarative = authFile != null;
+      declarative = false;
       openFirewall = false;
       config = baseConfig;
       web = {
@@ -40,8 +44,18 @@ lib.mkMerge [
         port = webPort;
         openFirewall = false;
       };
-    } // lib.optionalAttrs (authFile != null) {
-      inherit authFile;
+    };
+
+    systemd.services.deluged = lib.mkIf (authFile != null) {
+      preStart = lib.mkBefore ''
+        ${pkgs.coreutils}/bin/install -d -m 0770 -o ${username} -g ${primaryGroup} ${lib.escapeShellArg configDir}
+        ${pkgs.coreutils}/bin/cp ${generatedCoreConf} ${lib.escapeShellArg "${configDir}/core.conf"}
+        ${pkgs.coreutils}/bin/cp ${lib.escapeShellArg authFile} ${lib.escapeShellArg "${configDir}/auth"}
+        ${pkgs.coreutils}/bin/chown ${username}:${primaryGroup} ${lib.escapeShellArg "${configDir}/core.conf"} ${lib.escapeShellArg "${configDir}/auth"}
+        ${pkgs.coreutils}/bin/chmod 0600 ${lib.escapeShellArg "${configDir}/core.conf"} ${lib.escapeShellArg "${configDir}/auth"}
+      '';
+
+      serviceConfig.PermissionsStartOnly = true;
     };
   }
 
